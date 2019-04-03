a5719fe9c157df7711135504576be44ec964d2a0
Sandy Gifford <sanford.r.gifford@gmail.com>
2019-04-03 14:54:57 -0400

    bind to window

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as DnD5e from "dnd5e";
 
-const ClientEndpoints = new DnD5e.Endpoints(fetch, "dnd5e");
+const ClientEndpoints = new DnD5e.Endpoints(fetch.bind(window), "dnd5e");
 
 export { ClientEndpoints };
 export * from "dnd5e";
